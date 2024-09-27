ce6ef6fc8c29da972e1b479738439eaa6abdf292
AKSHAT SINGH PARMAR <141918847+akshat94116@users.noreply.github.com>
2024-09-28 02:00:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class Main
+{
+    public static void main(String as[]){
+    System.out.println("This is java program");
+    System.out.println("output from java program");
+    System.out.println(new Date());
+    }
+}
